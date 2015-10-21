8ca71caa586bb55ff02085366ef391e0a7f6da82
Leon Paternoster <leon.paternoster@zoho.com>
2015-10-21 20:44:27 +0100

    Breadcrumb code

---
+++ inc/breadcrumbs.php
@@ -0,0 +1,15 @@
+// Breadcrumbs plugin
+// This is only called on single pages
+
+
+function cs_custom_breadcrumbs() {
+
+    if ( in_category('news') ) {
+
+        $parent == 'News';
+        $parent_link == 'news';
+
+    }
+
+
+}
