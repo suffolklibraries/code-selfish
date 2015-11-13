91c0f79bdc71ef41af7e2bd2f56be4837ce84078
Leon Paternoster <leon.paternoster@suffolklibraries.co.uk>
2015-11-13 09:50:59 +0000

    Added fiction link to home page

--- custom-home.php
+++ custom-home.php
@@ -2,65 +2,209 @@
 
 <?php get_header(); ?>
 
-		<main id="content" class="clearfix" role="main">
+	<main id="content" class="clearfix" role="main">
 
-			<?php while (have_posts()) : the_post(); ?>
+		<?php while (have_posts()) : the_post(); ?>
 
-				<?php if ( has_post_thumbnail() ) {?>
+			<?php if ( has_post_thumbnail() ) {?>
 
-					<figure class="hero-splash hero-splash-<?php global $post; echo $post->post_name; ?>">
+				<figure class="hero-splash hero-splash-<?php global $post; echo $post->post_name; ?>">
 
-						<?php the_post_thumbnail(); ?>
+					<?php the_post_thumbnail(); ?>
 
-						<figcaption class="hero-splash-text hero-splash-text-<?php global $post; echo $post->post_name; ?>"><?php if ( has_excerpt() ) {?><?php the_excerpt(); ?><?php } ?></figcaption>
+					<figcaption class="hero-splash-text hero-splash-text-<?php global $post; echo $post->post_name; ?>"><?php if ( has_excerpt() ) {?><?php the_excerpt(); ?><?php } ?></figcaption>
 
-					</figure>
+				</figure>
 
-				<?php } ?>
+			<?php } ?>
 
-				<div class="wrapper">
+			<div class="wrapper">
 
-					<?php the_content(); ?>
+				<?php the_content(); ?>
 
-					<nav id="nav">
+				<nav id="nav">
 
-						<h1 class="hide accessibility">Navigation</h1>
+					<h1 class="hide accessibility">Navigation</h1>
+
+					<div class="g-row">
+
+						<div class="g-w-col4 g-w-left">
+
+							<section class="vbox">
+
+								<h1 class="vbox-title"><a href="#" class="icon icon-search">Search &amp; reserve</a></h1>
+
+								<p class="vbox-text">Search for books, CDs &amp; DVDs and reserve for collection from your library.</p>
+
+							</section>
+
+						</div>
+
+						<div class="g-w-col4 g-w-left">
+
+							<section class="vbox">
+
+								<h1 class="vbox-title"><a href="#" class="icon icon-lock">Login &amp; renew</a></h1>
+
+								<p class="vbox-text">See your loans &amp; fines, renew titles and update your details.</p>
+
+							</section>
+
+						</div>
+
+						<div class="g-w-col4 g-w-left">
+
+							<section class="vbox">
+
+		                        <h1 class="vbox-title"><a class="icon icon-tablet" href="#">eLibrary</a></h1>
+
+		                        <p class="vbox-text">Free eBooks, eAudio, downloads, magazines &amp; streaming.</p>
+
+		                    </section>
+
+						</div>
+
+					</div> <!-- end .g-row -->
+
+					<div class="g-row">
+
+		                <div class="g-w-col4 g-w-left">
+
+		                    <section class="vbox">
+
+		                        <h1 class="vbox-title"><a class="icon icon-map" href="#">Branches</a></h1>
+
+		                        <p class="vbox-text">Branch contact details and Ipswich Enterprise + Innovation hub.</p>
+
+		                    </section>
+
+		                </div>
+
+		                <div class="g-w-col4 g-w-left">
+
+		                    <section class="vbox">
+
+		                        <h1 class="vbox-title"><a class="icon icon-info" href="#">Help using the service</a></h1>
+
+		                        <p class="vbox-text">Guides to using the library services, costs &amp; what you can borrow.</p>
+
+		                    </section>
+
+		                </div>
+
+		                <div class="g-w-col4 g-w-left">
+
+							<section class="vbox">
+
+								<h1 class="vbox-title"><a href="#" class="icon icon-card">Apply for a card</a></h1>
+
+								<p class="vbox-text">Apply online to access books, music, CDs, research resources and the eLibrary.</p>
+
+							</section>
+
+		                </div>
+
+		            </div> <!-- end .g-row -->
+
+		            <div class="g-row">
+
+		                <div class="g-w-col4 g-w-left">
+
+							<section class="vbox">
+
+		                        <h1 class="vbox-title"><a class="icon icon-library" href="#">Information resources</a></h1>
+
+		                        <p class="vbox-text">Free access to Ancestry, Find My Past, Access to Research and many other resources.</p>
+
+		                    </section>
+
+		                </div>
+
+		                <div class="g-w-col4 g-w-left">
+
+							<section class="vbox">
+
+		                        <h1 class="vbox-title"><a class="icon icon-calendar" href="#">Events &amp; activities</a></h1>
+
+		                        <p class="vbox-text">Special events and regular activities for children &amp; older people. Free computer training.</p>
+
+		                    </section>
+
+		                </div>
+
+		                <div class="g-w-col4 g-w-left">
+
+							<section class="vbox">
+
+		                        <h1 class="vbox-title"><a class="icon icon-bus" href="#">Schools, mobiles, home</a></h1>
+
+		                        <p class="vbox-text">Mobile libraries, home library service &amp; Literacy, Books and Reading Team for Schools.</p>
+
+		                    </section>
+
+		                </div>
+
+		            </div> <!-- end .g-row -->
+
+					<div class="g-row">
+
+		                <div class="g-w-col4 g-w-left">
+
+							<section class="vbox">
+
+		                        <h1 class="vbox-title"><a class="icon icon-book" href="#">Reading</a></h1>
+
+		                        <p class="vbox-text">New in, suggestions, lists, groups and advice for children. Info on dyslexia and Irlens.</p>
+
+		                    </section>
+
+		                </div>
+
+		                <div class="g-w-col4 g-w-left">
+
+							<section class="vbox">
+
+		                        <h1 class="vbox-title"><a class="icon icon-pen" href="#">News &amp; blog</a></h1>
+
+		                        <p class="vbox-text">Announcements, press releases and our behind the scenes blog.</p>
+
+		                    </section>
+
+		                </div>
+
+						<div class="g-w-col4 g-w-left">
+
+							<section class="vbox">
+
+		                        <h1 class="vbox-title"><a class="icon icon-people" href="#">About us</a></h1>
+
+		                        <p class="vbox-text">Governance, board members, jobs and documents.</p>
+
+		                    </section>
+
+		                </div>
+
+		            </div> <!-- end .g-row -->
+
+				</nav>
+
+			</div> <!-- end .wrapper -->
+
+		<?php endwhile; ?>
+
+		<?php $the_query = new WP_Query( 'category_name=hot-picks&posts_per_page=6' ); ?>
+
+			<?php if ( $the_query->have_posts() ) : ?>
+
+				<section class="content-band clearfix" id="hot-picks">
+
+					<div class="wrapper">
 
 						<div class="g-row">
 
-							<div class="g-w-col4 g-w-left">
+							<div class="g-w-col12 g-w-left">
 
-								<section class="vbox">
-
-									<h1 class="vbox-title"><a href="#" class="icon icon-search">Search &amp; reserve</a></h1>
-
-									<p class="vbox-text">Search for books, CDs &amp; DVDs and reserve for collection from your library.</p>
-
-								</section>
-
-							</div>
-
-							<div class="g-w-col4 g-w-left">
-
-								<section class="vbox">
-
-									<h1 class="vbox-title"><a href="#" class="icon icon-lock">Login &amp; renew</a></h1>
-
-									<p class="vbox-text">See your loans &amp; fines, renew titles and update your details.</p>
-
-								</section>
-
-							</div>
-
-							<div class="g-w-col4 g-w-left">
-
-								<section class="vbox">
-
-			                        <h1 class="vbox-title"><a class="icon icon-tablet" href="#">eLibrary</a></h1>
-
-			                        <p class="vbox-text">Free eBooks, eAudio, downloads, magazines &amp; streaming.</p>
-
-			                    </section>
+								<h1 class="content-band-title">Hot picks</h1>
 
 							</div>
 
@@ -68,179 +212,83 @@
 
 						<div class="g-row">
 
-			                <div class="g-w-col4 g-w-left">
+							<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 
-			                    <section class="vbox">
+								<div class="g-w-left g-w-col2">
 
-			                        <h1 class="vbox-title"><a class="icon icon-map" href="#">Branches</a></h1>
+									<div class="nib-box secondary">
 
-			                        <p class="vbox-text">Branch contact details and Ipswich Enterprise + Innovation hub.</p>
+										<?php if ( has_post_thumbnail() ) : ?>
 
-			                    </section>
+							                <a href="<?php echo CFS()->get('catalogue_link'); ?>"><?php the_post_thumbnail(); ?></a>
 
-			                </div>
+										<?php endif; ?>
 
-			                <div class="g-w-col4 g-w-left">
+										<p><a href="<?php echo CFS()->get('catalogue_link'); ?>"><?php the_title(); ?></a></p>
 
-			                    <section class="vbox">
-
-			                        <h1 class="vbox-title"><a class="icon icon-info" href="#">Help using the service</a></h1>
-
-			                        <p class="vbox-text">Guides to using the library services, costs &amp; what you can borrow.</p>
-
-			                    </section>
-
-			                </div>
-
-			                <div class="g-w-col4 g-w-left">
-
-								<section class="vbox">
-
-									<h1 class="vbox-title"><a href="#" class="icon icon-card">Apply for a card</a></h1>
-
-									<p class="vbox-text">Apply online to access books, music, CDs, research resources and the eLibrary.</p>
-
-								</section>
-
-			                </div>
-
-			            </div> <!-- end .g-row -->
-
-			            <div class="g-row">
-
-			                <div class="g-w-col4 g-w-left">
-
-								<section class="vbox">
-
-			                        <h1 class="vbox-title"><a class="icon icon-library" href="#">Information resources</a></h1>
-
-			                        <p class="vbox-text">Free access to Ancestry, Find My Past, Access to Research and many other resources.</p>
-
-			                    </section>
-
-			                </div>
-
-			                <div class="g-w-col4 g-w-left">
-
-								<section class="vbox">
-
-			                        <h1 class="vbox-title"><a class="icon icon-calendar" href="#">Events &amp; activities</a></h1>
-
-			                        <p class="vbox-text">Special events and regular activities for children &amp; older people. Free computer training.</p>
-
-			                    </section>
-
-			                </div>
-
-			                <div class="g-w-col4 g-w-left">
-
-								<section class="vbox">
-
-			                        <h1 class="vbox-title"><a class="icon icon-bus" href="#">Schools, mobiles, home</a></h1>
-
-			                        <p class="vbox-text">Mobile libraries, home library service &amp; Literacy, Books and Reading Team for Schools.</p>
-
-			                    </section>
-
-			                </div>
-
-			            </div> <!-- end .g-row -->
-
-						<div class="g-row">
-
-			                <div class="g-w-col4 g-w-left">
-
-								<section class="vbox">
-
-			                        <h1 class="vbox-title"><a class="icon icon-book" href="#">Reading</a></h1>
-
-			                        <p class="vbox-text">New in, suggestions, lists, groups and advice for children. Info on dyslexia and Irlens.</p>
-
-			                    </section>
-
-			                </div>
-
-			                <div class="g-w-col4 g-w-left">
-
-								<section class="vbox">
-
-			                        <h1 class="vbox-title"><a class="icon icon-pen" href="#">News &amp; blog</a></h1>
-
-			                        <p class="vbox-text">Announcements, press releases and our behind the scenes blog.</p>
-
-			                    </section>
-
-			                </div>
-
-							<div class="g-w-col4 g-w-left">
-
-								<section class="vbox">
-
-			                        <h1 class="vbox-title"><a class="icon icon-people" href="#">About us</a></h1>
-
-			                        <p class="vbox-text">Governance, board members, jobs and documents.</p>
-
-			                    </section>
-
-			                </div>
-
-			            </div> <!-- end .g-row -->
-
-					</nav>
-
-				</div> <!-- end .wrapper -->
-
-			<?php endwhile; ?>
-
-			<?php $the_query = new WP_Query( 'category_name=hot-picks&posts_per_page=6' ); ?>
-
-				<?php if ( $the_query->have_posts() ) : ?>
-
-					<section class="content-band clearfix" id="hot-picks">
-
-						<div class="wrapper">
-
-							<div class="g-row">
-
-								<div class="g-w-col12 g-w-left">
-
-									<h1 class="content-band-title">Hot picks</h1>
+									</div> <!-- end .nib-box -->
 
 								</div>
 
-							</div> <!-- end .g-row -->
+							<?php endwhile; ?>
 
-							<div class="g-row">
+						</div> <!-- end .g-row -->
 
-								<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+					</div> <!-- end .wrapper -->
 
-									<div class="g-w-left g-w-col2">
+				</section>
 
-										<div class="nib-box secondary">
+			<?php endif; ?>
 
-											<?php if ( has_post_thumbnail() ) : ?>
+		<?php wp_reset_postdata(); ?>
 
-								                <a href="<?php echo CFS()->get('catalogue_link'); ?>"><?php the_post_thumbnail(); ?></a>
+		<section class="content-band clearfix" id="recommend">
 
-											<?php endif; ?>
+			<div class="wrapper">
 
-											<p><a href="<?php echo CFS()->get('catalogue_link'); ?>"><?php the_title(); ?></a></p>
+				<div class="g-row">
 
-										</div> <!-- end .nib-box -->
+					<div class="g-w-col12 g-w-left">
 
-									</div>
+						<h1 class="content-band-title">New in and recommendations</h1>
 
-								<?php endwhile; ?>
+					</div>
 
-							</div> <!-- end .g-row -->
+				</div> <!-- end .g-row -->
 
-						</div> <!-- end .wrapper -->
+				<div class="g-row">
 
-					</section>
+					<?php $the_query = new WP_Query( 'category_name=book-lists&posts_per_page=1' ); ?>
 
-				<?php endif; ?>
+					<?php if ( $the_query->have_posts() ) : ?>
 
-			<?php wp_reset_postdata(); ?>
+						<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+							<div class="g-w-left g-w-col2">
+
+								<div class="nib-box">
+
+									<h1 class="nib-box-title"><a href="category/book-lists">Fiction</a></h3>
+
+									<?php if ( has_post_thumbnail() ) : ?>
+
+										<a href="category/book-lists"><?php the_post_thumbnail(); ?></a>
+
+									<?php endif; ?>
+
+								</div> <!-- end .nib-box -->
+
+							  </div>
+
+						  <?php endwhile; ?>
+
+					 <?php endif; ?>
+
+					 <?php wp_reset_postdata(); ?>
+
+				</div> <!-- end .g-row -->
+
+			</div> <!-- end .wrapper -->
 
 		</section>
 
