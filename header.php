a43eac0910e8bcfe1b96de2a4b27e4ba1ec64176
Leon Paternoster <leon.paternoster@suffolklibraries.co.uk>
2015-12-21 13:28:45 +0000

    Added accessibility menu

--- header.php
+++ header.php
@@ -17,45 +17,67 @@
 
 <body <?php body_class(); ?>>
 
+	<div class="accessibility hide">
+
+		<ul>
+
+			<li><a href="#content">Skip to content</a></li>
+
+			<?php if ( is_front_page() ) {?>
+
+			<li><a href="#nav">Skip to main navigation menu</a></li>
+
+			<?php } ?>
+
+			<li><a href="#uni-nav">Skip to tools menu</a></li>
+
+		</ul>
+
+	</div>
+
 	<header class="topbar topbar-purple" id="site-header">
 
 		<div class="g-row">
 
-			<ul class="h-nav topbar-nav clearfix">
+			<nav id="uni-nav">
 
-				<li class="h-nav-item"><h1 class="topbar-title"><a class="h-nav-link" href="<?php echo esc_url( home_url( '/' ) ); ?>"><span class="weight-semib">Suffolk</span> <span class="weight-normal">Libraries</span></a></h1></li>
+				<ul class="h-nav topbar-nav clearfix">
 
-				<?php if ( ! is_front_page() ) {?>
+					<li class="h-nav-item"><h1 class="topbar-title"><a class="h-nav-link" href="<?php echo esc_url( home_url( '/' ) ); ?>"><span class="weight-semib">Suffolk</span> <span class="weight-normal">Libraries</span></a></h1></li>
 
-					<li class="h-nav-item"><a href="https://suffolk.spydus.co.uk/" class="h-nav-link icon icon-search-white">Search &amp; reserve</a></li>
-					<li class="h-nav-item"><a href="https://suffolk.spydus.co.uk/cgi-bin/spydus.exe/MSGTRN/OPAC/LOGINB" class="h-nav-link icon icon-lock-white">Login &amp; renew</a></li>
-					<li class="h-nav-item"><a href="https://suffolk.spydus.co.uk/cgi-bin/spydus.exe/MSGTRN/OPAC/JOIN" class="h-nav-link icon icon-card-white">Apply for a card</a></li>
-					<li class="h-nav-item"><a href="#" class="h-nav-link icon icon-info-white">Help</a></li>
-					<li class="h-nav-item"><a href="#" class="h-nav-link icon icon-phone-white">Contact</a></li>
+					<?php if ( ! is_front_page() ) {?>
 
-				<?php } ?>
+						<li class="h-nav-item"><a href="https://suffolk.spydus.co.uk/" class="h-nav-link icon icon-search-white">Search &amp; reserve</a></li>
+						<li class="h-nav-item"><a href="https://suffolk.spydus.co.uk/cgi-bin/spydus.exe/MSGTRN/OPAC/LOGINB" class="h-nav-link icon icon-lock-white">Login &amp; renew</a></li>
+						<li class="h-nav-item"><a href="https://suffolk.spydus.co.uk/cgi-bin/spydus.exe/MSGTRN/OPAC/JOIN" class="h-nav-link icon icon-card-white">Apply for a card</a></li>
+						<li class="h-nav-item"><a href="#" class="h-nav-link icon icon-info-white">Help</a></li>
+						<li class="h-nav-item"><a href="#" class="h-nav-link icon icon-phone-white">Contact</a></li>
 
-				<?php if ( is_front_page() ) {?>
+					<?php } ?>
 
-					<li class="h-nav-item"><a href="tel:01473263838" class="h-nav-link icon icon-phone-white">01473 263838</a></li>
-					<li class="h-nav-item"><a href="mailto:help@suffolklibraries.co.uk" class="h-nav-link icon icon-email-white">help@suffolklibraries.co.uk</a></li>
-					<li class="h-nav-item"><a href="http://twitter.com/suffolklibrary" class="h-nav-link icon icon-twitter-white">Twitter</a></li>
-					<li class="h-nav-item"><a href="http://facebook.com/suffolklibraries" class="h-nav-link icon icon-facebook-white">Facebook</a></li>
+					<?php if ( is_front_page() ) {?>
 
-				<?php } ?>
+						<li class="h-nav-item"><a href="tel:01473263838" class="h-nav-link icon icon-phone-white">01473 263838</a></li>
+						<li class="h-nav-item"><a href="mailto:help@suffolklibraries.co.uk" class="h-nav-link icon icon-email-white">help@suffolklibraries.co.uk</a></li>
+						<li class="h-nav-item"><a href="http://twitter.com/suffolklibrary" class="h-nav-link icon icon-twitter-white">Twitter</a></li>
+						<li class="h-nav-item"><a href="http://facebook.com/suffolklibraries" class="h-nav-link icon icon-facebook-white">Facebook</a></li>
 
-				<li class="h-nav-item">
+					<?php } ?>
 
-					<form id="searchform" class="pure-form pure-form-custom" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
+					<li class="h-nav-item">
 
-						<input id="s" type="search" name="s" value=""></input>
-						<input id="searchsubmit" type="submit" class="pure-button pure-button-custom" value="Search"></input>
+						<form id="searchform" class="pure-form pure-form-custom" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
 
-					</form>
+							<input id="s" type="search" name="s" value=""></input>
+							<input id="searchsubmit" type="submit" class="pure-button pure-button-custom" value="Search"></input>
 
-				</li>
+						</form>
 
-			</ul>
+					</li>
+
+				</ul>
+
+			</nav> <!-- end #uni-nav -->
 
 		</div> <!-- end .g-row -->
 
