ea89e8929aeb998e397861dcb7cd0fe67548c2ad
suffolklibraries <leon.paternoster@suffolklibraries.co.uk>
2016-02-05 15:19:59 +0000

    Changed single-location template

--- single-location.php
+++ single-location.php
@@ -1,75 +1,71 @@
 <?php get_header(); ?>
 
-	<?php while ( have_posts() ) : the_post(); ?>
+	<main id="content" role="main">
 
-		<div class="wrapper">
+		<?php while ( have_posts() ) : the_post(); ?>
 
-			<article <?php post_class( 'bg bg-white') ?> id="post-<?php the_ID(); ?>">
+			<div class="inner-wrapper">
 
-				<div class="g-row">
+				<article <?php post_class( 'bg bg-white') ?> id="post-<?php the_ID(); ?>">
 
-					<div class="g-w-col12">
+					<div class="g-row">
 
-						<header>
+						<div class="g-w-col12">
 
-							<?php if(function_exists('breadcrumb_trail')) { breadcrumb_trail(array('separator' => ' &rarr; ', 'show_browse' => 'false')); } ?>
+							<header class="entry-header single-narrow-header single-narrow-header">
 
-							<h1 class="entry-title"><?php the_title(); ?></h1>
+								<?php if(function_exists('breadcrumb_trail')) { breadcrumb_trail(array('separator' => ' &rarr; ', 'show_browse' => 'false')); } ?>
 
-							<?php
+								<h1 class="entry-title single-narrow-title single-narrow-title"><?php the_title(); ?></h1>
 
-								// check if the post has a Post Thumbnail assigned to it.
-								if ( has_post_thumbnail() ) {
-									echo "<figure>";
-									the_post_thumbnail();
-									echo "<figcaption class='secondary'><p>";
-									echo get_post(get_post_thumbnail_id())->post_excerpt;
-									echo "</p></figcaption>";
-									echo "</figure>";
-								}
-
-							?>
-
-						</header>
-
-					</div>
-
-				</div> <!-- end .g-row -->
-
-				<div class="g-row">
-
-					<div class="entry-content">
-
-						<div class="g-w-col6 g-w-left">
-
-							<section class="content-band">
-
-								<h1 class="content-band-title">Information</h1>
-
-								<p><strong>Free wifi available</strong></p>
-
-								<?php the_content(); ?>
-
-							</section>
-
-							<section class="content-band">
-
-								<h1 class="content-band-title">Special events</h1>
-
-								<?php echo EM_Events::output(array('limit'=>30, 'location'=>$EM_Location->location_id, 'pagination'=>1)); ?>
-
-							</section>
+							</header>
 
 						</div>
 
 					</div> <!-- end .g-row -->
 
-				</div>
+					<div class="g-row">
 
-			</article>
+						<div class="entry-content single-narrow-content single-narrow-content">
 
-		</div>  <!-- end .wrapper -->
+							<div class="g-w-col12">
 
-	<?php endwhile; // End the loop ?>
+								<?php
+
+									// check if the post has a Post Thumbnail assigned to it.
+									if ( has_post_thumbnail() ) {
+										echo "<figure>";
+										the_post_thumbnail();
+										echo "<figcaption class='secondary'><p>";
+										echo get_post(get_post_thumbnail_id())->post_excerpt;
+										echo "</p></figcaption>";
+										echo "</figure>";
+									}
+
+								?>
+
+								<?php the_content(); ?>
+
+								<section class="content-band" id="library-content">
+
+										<h1 class="content-band-title">Special events</h1>
+
+										<?php echo EM_Events::output(array('limit'=>30, 'location'=>$EM_Location->location_id, 'pagination'=>1)); ?>
+
+								</section>
+
+							</div>
+
+						</div> <!-- end entry-content style -->
+
+					</div> <!-- end .g-row -->
+
+				</article>
+
+			</div>  <!-- end .inner-wrapper -->
+
+		<?php endwhile; // End the loop ?>
+
+	</main>
 
 <?php get_footer(); ?>
